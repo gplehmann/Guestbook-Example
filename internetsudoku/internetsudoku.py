a8b7f10d2017a1c06bf613ae7e095bedc5282117
Greg Lehmann <gregftw@gmail.com>
2011-10-18 00:53:08 -0600

    Added Websudoku class, HTML sudoku puzzle parsing, and PuzzleParse exception

--- internetsudoku/internetsudoku.py
+++ internetsudoku/internetsudoku.py
@@ -28,7 +28,12 @@ class Puzzle(db.Model):
     date = db.DateTimeProperty(auto_now_add=True)
 
 
+class PuzzleParseError(Exception): pass
+    """Raised if a parsed puzzle does not produce a valid 9x9 grid"""
+
+
 def puzzle_key(puzzle_name=None):
+    """Returns a datastore key for a Puzzle entity"""
     return db.Key.from_path('Puzzle', puzzle_name or 'default_puzzle')
 
 
@@ -47,19 +52,98 @@ class MainPage(webapp.RequestHandler):
         self.response.out.write(template.render(path, template_values))
 
 #TODO: add a self parameter if this gets moved into a class
-def pull_websudoku(difficulty=1)
-    puzzle_page = urllib2.urlopen('http://view.websudoku.com/?level=' + difficulty)
-    puzzle_regex = re.compile('<TABLE.*?>(<TR>>(<TD.*?><INPUT.*?></TD>){9}</TR>){9}</TABLE>', re.IGNORECASE)
-    puzzle_HTML = ''
-
-    #Find the line from the page that contains the HTML code for the puzzle
-    for line in puzzle_page:
-        if puzzle_regex.match(line):
-            puzzle_HTML = line
-            break
-
-    #TODO: Format the HTML into a 9x9 list matrix
+class Websudoku:
+    puzzle = []
+    URL = ''
+    difficulty = 1
+    def __init__(self, difficulty=1, URL='http://view.websudoku.com/'):
+        self.URL = URL
+        self.difficulty = difficulty
+        self.puzzle = self.sudoku_html_to_list(self.pull_sudoku())
     
+    def pull_sudoku(self)
+        puzzle_page = urllib2.urlopen(self.URL + '?level=' + str(self.difficulty))
+        puzzle_regex = re.compile('<TABLE.*?>(<TR>(<TD.*?><INPUT.*?></TD>){9}</TR>){9}</TABLE>', re.IGNORECASE)
+        puzzle_HTML = ''
+
+        #Find the line from the page that contains the HTML code for the puzzle
+        for line in puzzle_page:
+            if puzzle_regex.match(line):
+                puzzle_HTML = line.strip()
+                break
+
+        if puzzle_HTML == '':
+            raise PuzzleParseError, ('Unable to pull a valid HTML representation of a sudoku puzzle',)
+        return puzzle_HTML
+
+    def sudoku_html_to_list(self, html_str):
+        empty_regex = re.compile('<INPUT.*?onBlur.*?>', re.IGNORECASE)
+        full_regex = re.compile('<INPUT.*?READONLY VALUE="(\d)".*?>', re.IGNORECASE)
+
+        #Remove extraneous HTML and change <TD> tags into a delimiter we can split on
+        html_str = re.sub(re.compile('</?TABLE.*?>|</?TR>', re.IGNORECASE), '', html_str)
+        html_str = re.sub(re.compile('</?TD.*?>', re.IGNORECASE), '|', html_str)
+        html_str = html_str.split('|')
+        puzzle = [[]]
+        row = 0
+        col = 0
+        counter = 0
+
+        #parse the HTML and convert <INPUT> tags into either empty or full cells in a 9x9 sudoku grid
+        for cell in html_str:
+            if cell:
+                empty_match = empty_regex.match(cell)
+                full_match = full_regex.match(cell)
+                if empty_match:
+                    puzzle[row].append('0')
+                elif full_match:
+                    puzzle[row].append(full_match.group(1))
+                else:
+                    raise PuzzleParseError, ('Puzzle has an invalid cell at position ' + str(row) + "," + str(col), )
+                col += 1
+                if col > 8:
+                    col = 0
+                    row += 1
+                    if row <= 8:
+                        puzzle.append([])
+
+                counter += 1
+
+        if counter != 81:
+            raise PuzzleParseError, ('Puzzle derived from the HTML parameter was not a 9x9 grid',)
+
+        return puzzle
+
+
+    def store_puzzle(self, puzzle_name):
+        #Convert puzzle into nine 9-character strings for storage
+        entity_row = []
+        for row in self.puzzle:
+            entity_row.append(str(row).strip('[],\' '))
+
+        if self.difficulty == 1:
+            str_difficulty = 'EASY'
+        elif self.difficulty == 2:
+            str_difficulty = 'MEDIUM'
+        elif self.difficulty == 3:
+            str_difficulty = 'HARD'
+        elif self.difficulty == 4:
+            str_difficulty = 'EVIL'
+        else:
+            str_difficulty = 'UNKNOWN'
+            
+        puzzle_entity = Puzzle(parent=puzzle_key(puzzle_name), difficulty=str_difficulty)
+        puzzle_entity.row_1 = entity_row[0]
+        puzzle_entity.row_2 = entity_row[1]
+        puzzle_entity.row_3 = entity_row[2]
+        puzzle_entity.row_4 = entity_row[3]
+        puzzle_entity.row_5 = entity_row[4]
+        puzzle_entity.row_6 = entity_row[5]
+        puzzle_entity.row_7 = entity_row[6]
+        puzzle_entity.row_8 = entity_row[7]
+        puzzle_entity.row_9 = entity_row[8]
+        puzzle_entity.put()
+
 
 application = webapp.WSGIApplication(
                                      [('/', MainPage)],
